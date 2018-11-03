c46eba137fadebd0482e4b71c788315ce0df4b91
mldn <mldnqa@163.com>
2018-11-03 17:12:01 +0800

    try

--- Java01.java
+++ Java01.java
@@ -5,6 +5,9 @@ import java.text.*;
 inteface IMessage{
 	public void getMessage();
 }
+inteface IMessage{
+	public void getMessage();
+}
 public class Java01{
 	public static void main(String[] args) throws Exception{
 		IMessage tmp = MessageUtil.getMessage().get();
