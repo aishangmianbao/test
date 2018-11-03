a7f12c0c79ae1d649d6adf07675b85554a5c9cb7
mldn <mldnqa@163.com>
2018-11-03 17:05:35 +0800

    change  Java01.java

--- Java01.java
+++ Java01.java
@@ -2,7 +2,9 @@ import java.util.*;
 import java.lang.*;
 import java.text.*;
 
-
+inteface IMessage{
+	public void getMessage();
+}
 public class Java01{
 	public static void main(String[] args) throws Exception{
 		IMessage tmp = MessageUtil.getMessage().get();
